ea55ab36304fea9da6ea75c2028dd47b35383d64
Tristan Salinas <salinas.tristan@gmail.com>
2024-11-24 22:45:01 +0100

    24.11.24 end of day

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,13 +1,13 @@
-import { ProductList } from "../../types/products"
-import { Product } from "../../types/products"
-import { ProductCard } from "../ProductCard/ProductCard"
+import { ProductList } from "../../types/products";
+import { Product } from "../../types/products";
+import { ProductCard } from "../ProductCard/ProductCard";
 
-export function ProductGrid({products }: ProductList ){
-  return(
+export function ProductGrid({ products }: ProductList) {
+  return (
     <div className="product-grid">
-      {products.map((_product : Product)=>{
-        return <ProductCard product= {_product} />
+      {products.map((_product: Product) => {
+        return <ProductCard key={_product.id} product={_product} />;
       })}
     </div>
-  )
+  );
 }
