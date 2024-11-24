0c28ee41c03bd6654774c7b24a2cb3ffb0fcc300
Tristan Salinas <salinas.tristan@gmail.com>
2024-11-24 21:42:15 +0100

    nonFonctionnel, pour review3

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,21 +1,30 @@
 import { Product } from "../../types/products";
 import { useCart } from "../../context/CartContext";
-import { useState } from "react";
+//import { useState } from "react";
 interface ProductCardProps {
   product: Product; // Expecting a Product object
 }
 
 export function ProductCard({ product }: ProductCardProps) {
   const cart = useCart();
-  const [countInCart, setCountInCart] = useState<number>(0);
+
+  const handleAdd = () => {
+    cart.addToCart(product);
+  };
+  const handleMinus = () => {
+    cart.updateQuantity(product, "REMOVE");
+  };
+  const handlePlus = () => {
+    cart.updateQuantity(product, "ADD");
+  };
 
   return (
     <article className="product-card">
       <p>{product.name}</p>
-      {countInCart <= 0 ? (
+      {cart.countInCartOf(product) <= 0 ? (
         <button
           className="product-card-btn product-card-btn--add"
-          onClick={useCart().addToCart}
+          onClick={handleAdd}
         >
           ADD TO CART
         </button>
@@ -24,7 +33,7 @@ export function ProductCard({ product }: ProductCardProps) {
           <button className="product-card-btn__minus" onClick={handleMinus}>
             -
           </button>
-          <p>{countInCart}</p>
+          <p>{cart.countInCartOf(product)}</p>
           <button className="product-card-btn__plus" onClick={handlePlus}>
             +
           </button>
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -35,6 +35,7 @@ interface UseCartReturn {
   addToCart: (product: Product) => void;
   removeFromCart: (product: Product) => void;
   updateQuantity: (product: Product, operation: "ADD" | "REMOVE") => void;
+  countInCartOf: (product: Product) => number;
   countInCart: number;
 }
 
@@ -83,86 +84,20 @@ export function useCart(): UseCartReturn {
     }
   };
 
+  const countInCartOf = (product: Product) => {
+    const cartProduct = cartProducts.find(
+      (cartProduct) => cartProduct.id === product.id
+    );
+    return cartProduct ? cartProduct.quantity : 0;
+  };
+
   const countInCart = cartProducts.length;
   return {
-    cartProducts: cartProducts,
-    addToCart: addToCart,
-    removeFromCart: removeFromCart,
-    updateQuantity: updateQuantity,
+    cartProducts,
+    addToCart,
+    removeFromCart,
+    updateQuantity,
+    countInCartOf,
     countInCart,
   };
 }
-
-/*export type CartAction =
-  | { type: "ADD_ITEM"; payload: Product }
-  | { type: "REMOVE_ITEM"; payload: { id: number } }
-  | { type: "UPDATE_QUANTITY"; payload: { id: number; quantity: number } };
-
-interface CartItem extends Product {
-  quantity: number;
-}
-
-interface CartProductList {
-  cartItems: CartItem[];
-}
-
-const initialState: CartProductList = {
-  cartItems: [],
-};
-
-const cartReducer = (
-  state: CartProductList,
-  action: CartAction
-): CartProductList => {
-  switch (action.type) {
-    case "ADD_ITEM":
-      return {
-        ...state,
-        cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
-      };
-    case "REMOVE_ITEM":
-      return {
-        ...state,
-        cartItems: state.cartItems.filter(
-          (cartItem) => cartItem.id !== action.payload.id
-        ),
-      };
-    case "UPDATE_QUANTITY":
-      return {
-        ...state,
-        cartItems: state.cartItems.map((cartItem) =>
-          cartItem.id === action.payload.id
-            ? { ...cartItem, quantity: action.payload.quantity }
-            : cartItem
-        ),
-      };
-    default:
-      return state;
-  }
-};
-
-const CartContext = createContext<
-  | {
-      state: CartProductList;
-      dispatch: React.Dispatch<CartAction>;
-    }
-  | undefined
->(undefined);
-
-export function CartProvider({ children }: { children: ReactNode }) {
-  const [state, dispatch] = useReducer(cartReducer, initialState);
-  return (
-    <CartContext.Provider value={{ state, dispatch }}>
-      {children}
-    </CartContext.Provider>
-  );
-}
-
-export const useCartContext = () => {
-  const context = useContext(CartContext);
-  if (!context) {
-    throw new Error("useCart must be used within a CartProvider");
-  }
-  return context;
-};
-*/
