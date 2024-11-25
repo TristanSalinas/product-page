3ca0896cff198ffc7be737af619d7d01cfd7985c
Tristan Salinas <salinas.tristan@gmail.com>
2024-11-25 10:05:01 +0100

    testing deploy.yml

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,7 +1,15 @@
+import { useCart } from "../../context/CartContext";
+
 export function Cart() {
+  const carts = useCart();
   return (
     <div className="cart">
       <h2>Your Cart</h2>
+      {carts.cartProducts.map((cartItem) => (
+        <p key={cartItem.id}>
+          {cartItem.name} : {cartItem.quantity}
+        </p>
+      ))}
     </div>
-  )
+  );
 }
