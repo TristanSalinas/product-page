8bc228edfe5794598201a7b78598438884087e3d
Tristan Salinas <salinas.tristan@gmail.com>
2024-11-25 10:55:08 +0100

    testing viteconfig

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -4,7 +4,7 @@ export function Cart() {
   const carts = useCart();
   return (
     <div className="cart">
-      <h2>Your Cart</h2>
+      <h2>Your Cart :P</h2>
       {carts.cartProducts.map((cartItem) => (
         <p key={cartItem.id}>
           {cartItem.name} : {cartItem.quantity}
